4d97e10faa63d364dafd900612fad06aca420aa0
Erica Schwartz <Erica_S.Schwartz@tufts.edu>
2017-08-05 22:22:08 -0400

    finished lab1.py

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -17,7 +17,7 @@ from production import IF, AND, OR, NOT, THEN, forward_chain
 #    2. the consequent
 #    3. both
 
-ANSWER_1 = 'your answer here'
+ANSWER_1 = 2
 
 # A rule-based system about Monty Python's "Dead Parrot" sketch
 # uses the following rules:
@@ -37,10 +37,10 @@ ANSWER_1 = 'your answer here'
 
 # Will this system produce the datum 'Polly is pining for the
 # fjords'?  Answer 'yes' or 'no'.
-ANSWER_2 = 'your answer here'
+ANSWER_2 = 'no'
 
 # Which rule contains a programming error? Answer '1' or '2'.
-ANSWER_3 = 'your answer here'
+ANSWER_3 = '2'
 
 # If you're uncertain of these answers, look in tests.py for an
 # explanation.
@@ -69,11 +69,11 @@ ANSWER_3 = 'your answer here'
 # what is asked.  After we start the system running, which rule
 # fires first?
 
-ANSWER_4 = 'your answer here'
+ANSWER_4 = '1'
 
 # Which rule fires second?
 
-ANSWER_5 = 'your answer here'
+ANSWER_5 = "neither; once rule 1 fires, no rule's THEN changes the data"
 
 
 # Problem 1.3.1: Poker hands
@@ -90,7 +90,8 @@ poker_data = ( 'two-pair beats pair',
 # which poker hands beat which, transitively. For example, it
 # should be able to deduce that a three-of-a-kind beats a pair,
 # because a three-of-a-kind beats two-pair, which beats a pair.
-transitive_rule = IF( AND(), THEN() )
+transitive_rule = IF( AND('(?x) beats (?y)', '(?y) beats (?z)'),
+                      THEN('(?x) beats (?z)'))
 
 # You can test your rule like this:
 # print forward_chain([transitive_rule], poker_data)
@@ -114,7 +115,46 @@ TEST_RESULTS_TRANS2 = forward_chain([transitive_rule],
 
 # Then, put them together into a list in order, and call it
 # family_rules.
-family_rules = [ ]                    # fill me in
+family_rules = [
+    IF ('male (?x)', THEN('same (?x) (?x)')),
+    IF ('female (?x)', THEN('same (?x) (?x)')),
+    IF (
+        AND('sibling (?x) (?y)', 'male (?y)'),
+        THEN('brother (?y) (?x)')),
+    IF (
+        AND('sibling (?x) (?y)', 'female (?y)'),
+        THEN('sister (?y) (?x)')),
+    IF (
+        AND('parent (?x) (?y)', 'female (?x)'),
+        THEN('mother (?x) (?y)')),
+    IF (
+        AND('parent (?x) (?y)', 'male (?x)'),
+        THEN('father (?x) (?y)')),
+    IF (
+        AND('parent (?x) (?y)', 'male (?y)'),
+        THEN('son (?y) (?x)')),
+    IF (
+        AND('parent (?x) (?y)', 'female (?y)'),
+        THEN('daughter (?y) (?x)')),
+    IF (
+        AND('parent (?x) (?y)', 'parent (?x) (?z)', NOT('same (?y) (?z)')),
+        THEN('sibling (?y) (?z)')),
+    IF (
+        AND('parent (?x) (?y)', 'parent (?x) (?z)', NOT('same (?y) (?z)')),
+        THEN('sibling (?z) (?y)')),
+    IF (
+        AND('parent (?a) (?b)', 'parent (?c) (?d)', 'sibling (?a) (?c)'),
+        THEN('cousin (?b) (?d)')),
+    IF (
+        AND('parent (?a) (?b)', 'parent (?c) (?d)', 'sibling (?a) (?c)'),
+        THEN('cousin (?d) (?b)')),
+    IF (
+        AND('parent (?x) (?y)', 'parent (?y) (?z)'),
+        THEN('grandparent (?x) (?z)')),
+    IF (
+        AND('parent (?x) (?y)', 'parent (?y) (?z)'),
+        THEN('grandchild (?z) (?x)')),
+]
 
 # Some examples to try it on:
 # Note: These are used for testing, so DO NOT CHANGE
