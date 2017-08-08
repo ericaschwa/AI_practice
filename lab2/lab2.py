705c92c14bb792a2443af1b7c09edacf1369b7a4
Erica Schwartz <Erica_S.Schwartz@tufts.edu>
2017-08-08 11:26:32 -0400

    did first three searches and other lab questions; working on beam

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -7,28 +7,28 @@
 
 # 1: True or false - Hill Climbing search is guaranteed to find a solution
 #    if there is a solution
-ANSWER1 = None
+ANSWER1 = False
 
 # 2: True or false - Best-first search will give an optimal search result
 #    (shortest path length).
 #    (If you don't know what we mean by best-first search, refer to
 #     http://courses.csail.mit.edu/6.034f/ai3/ch4.pdf (page 13 of the pdf).)
-ANSWER2 = None
+ANSWER2 = False
 
 # 3: True or false - Best-first search and hill climbing make use of
 #    heuristic values of nodes.
-ANSWER3 = None
+ANSWER3 = True
 
 # 4: True or false - A* uses an extended-nodes set.
-ANSWER4 = None
+ANSWER4 = True
 
 # 5: True or false - Breadth first search is guaranteed to return a path
 #    with the shortest number of nodes.
-ANSWER5 = None
+ANSWER5 = True
 
 # 6: True or false - The regular branch and bound uses heuristic values
 #    to speed up the search for an optimal path.
-ANSWER6 = None
+ANSWER6 = False
 
 # Import the Graph data structure from 'search.py'
 # Refer to search.py for documentation
@@ -40,19 +40,52 @@ from search import Graph
 # The online tester will not test them.
 
 def bfs(graph, start, goal):
-    raise NotImplementedError
+    paths  = [[start]]
+    while len(paths) > 0 and paths[-1][-1] != goal:
+        path      = paths.pop()
+        new_paths = []
+        for node in graph.get_connected_nodes(path[-1]):
+            if node not in path:
+                new_paths[:0] = [path + [node]]
+        paths   = new_paths + paths
+    if len(paths) > 0 and paths[-1][-1] == goal:
+        return paths[-1]
+    else:
+        return []
 
 ## Once you have completed the breadth-first search,
 ## this part should be very simple to complete.
 def dfs(graph, start, goal):
-    raise NotImplementedError
-
+    paths = [[start]]
+    while len(paths) > 0 and paths[-1][-1] != goal:
+        path      = paths.pop()
+        new_paths = []
+        for node in graph.get_connected_nodes(path[-1]):
+            if node not in path:
+                new_paths.append(path + [node])
+        paths += new_paths
+    if paths[-1][-1] == goal:
+        return paths[-1]
+    else:
+        return []
 
 ## Now we're going to add some heuristics into the search.  
 ## Remember that hill-climbing is a modified version of depth-first search.
 ## Search direction should be towards lower heuristic values to the goal.
 def hill_climbing(graph, start, goal):
-    raise NotImplementedError
+    paths = [[start]]
+    while len(paths) > 0 and paths[-1][-1] != goal:
+        path      = paths.pop()
+        new_paths = []
+        for node in graph.get_connected_nodes(path[-1]):
+            if node not in path:
+                new_paths.append(path + [node])
+        paths += sorted(new_paths,
+                        key=lambda p: graph.get_heuristic(p[-1], goal)*-1)
+    if paths[-1][-1] == goal:
+        return paths[-1]
+    else:
+        return []
 
 ## Now we're going to implement beam search, a variation on BFS
 ## that caps the amount of memory used to store paths.  Remember,
@@ -60,7 +93,25 @@ def hill_climbing(graph, start, goal):
 ## The k top candidates are to be determined using the 
 ## graph get_heuristic function, with lower values being better values.
 def beam_search(graph, start, goal, beam_width):
-    raise NotImplementedError
+# TODO: can have up to k paths AT EACH LEVEL
+    paths  = [[start]]
+    levels = {start: 1}
+    while len(paths) > 0 and paths[-1][-1] != goal:
+        path      = paths.pop()
+        new_paths = []
+        for node in graph.get_connected_nodes(path[-1]):
+            if node not in levels:
+                levels[node] = levels[path[-1]] + 1
+            if node not in path: # and len(path) <= levels[path[-1]]:
+                new_paths[:0] = [path + [node]]
+        keepers = sorted(new_paths + paths,
+                         key=lambda p: graph.get_heuristic(p[-1], goal)*-1)
+        keepers = keepers[len(keepers) - beam_width:]
+        paths   = [p for p in new_paths + paths if p in keepers]
+    if len(paths) > 0 and paths[-1][-1] == goal:
+        return paths[-1]
+    else:
+        return []
 
 ## Now we're going to try optimal search.  The previous searches haven't
 ## used edge distances in the calculation.
@@ -89,6 +140,6 @@ def is_admissible(graph, goal):
 def is_consistent(graph, goal):
     raise NotImplementedError
 
-HOW_MANY_HOURS_THIS_PSET_TOOK = ''
-WHAT_I_FOUND_INTERESTING = ''
-WHAT_I_FOUND_BORING = ''
+HOW_MANY_HOURS_THIS_PSET_TOOK = '4'
+WHAT_I_FOUND_INTERESTING = 'Writing the search functions.'
+WHAT_I_FOUND_BORING = 'Answering these questions.'
